a013c06acb96086887a76888b4d27ed2441737a7
clausti and kyungmin <clausti@gmail.com>
2013-10-22 11:45:18 -0700

    Finish add_numbers

---
+++ add_numbers.js
@@ -0,0 +1,26 @@
+var readline = require('readline');
+
+reader = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
+
+var addNumbers = function (sum, numsLeft, completionCallback) {
+	if (numsLeft > 0) {
+		reader.question("What's your number?", function (num) {
+			var num1 = parseInt(num);
+			sum += num1;
+			console.log(sum);
+			numsLeft--;
+			addNumbers(sum, numsLeft, completionCallback);
+		});
+	} else if (numsLeft === 0) {
+		completionCallback(sum);
+		reader.close();
+	}
+}
+
+addNumbers(0, 3, function(sum) {
+	console.log("Total Sum: " + sum );
+});
+
